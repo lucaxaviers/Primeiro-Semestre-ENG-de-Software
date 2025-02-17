913db42b015c1a05660f10558b57abbb438f25d9
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-02-17 11:28:56 -0300

    Add files via upload

---
+++ verOResto.py
@@ -0,0 +1,5 @@
+numero=int(input("digite o numero que vai dividido "))
+divisor=int(input("digite o divisor "))
+conta=numero/divisor
+print("o resultado é ",conta)
+print("\n o resto é ",numero%divisor)
