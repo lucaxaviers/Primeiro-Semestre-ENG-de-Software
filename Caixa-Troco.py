30f59a18276d502d16291a2061b4f1f1ae3ad83b
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-02-23 18:58:52 -0300

    Criar Caixa-Troco.py

---
+++ Caixa-Troco.py
@@ -0,0 +1,27 @@
+# Solicita os valores
+recebido = int(input("Valor recebido: R$"))
+compra = int(input("Valor da compra: R$"))
+
+# Calcula o troco
+troco = recebido - compra
+
+# Verifica se o valor é suficiente
+if troco < 0:
+    print("Valor insuficiente. Não há troco.")
+else:
+    print(f"Troco a ser dado: R${troco}")
+    
+    cedulas = [100, 50, 20, 10, 5, 2, 1]
+    print("Troco em notas:")
+
+    # Calcula o troco em notas
+    for cedula in cedulas:
+        qtd = troco // cedula
+        troco %= cedula
+
+        if qtd > 0:
+            print(f"{qtd} nota(s) de {cedula}")
+    
+    # Caso sobrem centavos
+    if troco > 0:
+        print(f"Não foi possível entregar o troco completo. Sobrou {troco} centavos.")
