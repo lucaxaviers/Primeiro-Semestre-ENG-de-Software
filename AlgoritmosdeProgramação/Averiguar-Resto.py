6d92fb9596eda758b38d05d15979ba25f0bb5817
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-02-24 16:37:56 -0300

    Atualizar o Averiguar-Resto.py

--- AlgoritmosdeProgramação/Averiguar-Resto.py
+++ AlgoritmosdeProgramação/Averiguar-Resto.py
@@ -1,5 +1,8 @@
 Ver o Resto da Divisão em Python
 -------------------------------------------------------
+# Exibe o título do cálculo
+print("DESCUBRINDO O RESULTADO E O RESTO DA DIVISÃO")
+
 # Solicita o número e o divisor
 numero = int(input("Digite o número que vai ser dividido: "))
 divisor = int(input("Digite o divisor: "))
@@ -10,4 +13,3 @@ conta = numero / divisor
 # Exibe o resultado e o resto
 print("O resultado é", conta)
 print("\nO resto é", numero % divisor)
-
