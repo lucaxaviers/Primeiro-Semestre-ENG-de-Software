0ce46cb4e9fc114000dd42dadae2867b668bc9d8
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-02-24 08:08:40 -0300

    Update AreaPerimetro.py

--- AreaPerimetro.py
+++ AreaPerimetro.py
@@ -1,7 +1,13 @@
 Calculando a Área e o Perímetro de um quadrado em Phyton
 ---------------------------------------------------------------------
-lado=float(input("Digite o valor do lado=  "))
-area=(lado*lado)
-perimetro=(4*lado)
-print("Area= "area)
-print("Perimetro= "perimetro)              
+# Solicita o valor do lado do quadrado
+lado = float(input("Digite o valor do lado= "))
+
+# Calcula a área e o perímetro do quadrado
+area = (lado * lado)
+perimetro = (4 * lado)
+
+# Exibe a área e o perímetro
+print("Área =", area)
+print("Perímetro =", perimetro)
+
