0dda285fc8eff188f54a030bed40b2fc461e38d9
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-02-24 10:53:14 -0300

    Add files via upload

---
+++ AlgoritmosdeProgramação/conversor.py
@@ -0,0 +1,5 @@
+valor=float(input("Digite o valor em Dólar= "))
+print("Caso não saiba considere 5.726")
+cotacao=float(input("Digite a Cotação do Dólar= "))
+print(f"O valor em Reais é {valor * cotacao:.2f}")
+            
