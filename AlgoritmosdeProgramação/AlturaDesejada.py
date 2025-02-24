82dbc940f46af5222d6a6f017f7fa7b469bfda4c
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-02-24 11:33:28 -0300

    Add files via upload

---
+++ AlgoritmosdeProgramação/AlturaDesejada.py
@@ -0,0 +1,4 @@
+print("CALCULO DE QUANTOS DEGRAUS VOCÊ VAI SUBIR")
+degrau=float(input("qual a altura do degrau da escada= "))
+altura=float(input("qual altura você quer chegar= "))
+print(f"Você vai subir {altura/degrau:.2f} degraus para chegar na altura desejada ")
