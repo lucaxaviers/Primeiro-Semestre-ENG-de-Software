80c6d347aa19b34cb06b89fed3a83974d8b80920
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-02-24 11:15:52 -0300

    Add files via upload

---
+++ AlgoritmosdeProgramação/DescontoSalarial.py
@@ -0,0 +1,8 @@
+dias=int(input("Digite a quantidade de Dias que o Encanador trabalhou= "))
+bruto=dias*30
+desconto=bruto*0.08
+liquido=bruto-desconto
+print(f"Salario Bruto: R$",bruto)
+print(f"Desconto: R$ ",desconto)
+print(f"Salario Liquido:R$ ",liquido)
+
