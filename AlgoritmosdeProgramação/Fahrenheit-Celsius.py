7f6abb882bf00c78e4bd896d04e4c371633f9dde
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-02-24 10:41:25 -0300

    Update Fahrenheit-Celsius.py

--- AlgoritmosdeProgramação/Fahrenheit-Celsius.py
+++ AlgoritmosdeProgramação/Fahrenheit-Celsius.py
@@ -1,3 +1,7 @@
-Fahrenheit=float(input("digite a temperatura em °F = "))
-Celsius=(Fahrenheit - 32) * 5/9
-print(f"A temperatura em Celsius é: ",(Fahrenheit - 32) * 5/9)
+Fahrenheit = float(input("Digite a temperatura em °F = "))
+Celsius = (Fahrenheit - 32) * 5 / 9
+print(f"A temperatura em Celsius é: {Celsius}")
+
+
+
+
