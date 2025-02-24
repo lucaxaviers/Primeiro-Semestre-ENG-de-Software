10651295494f857aaec1bee7d61186980b256c70
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-02-24 16:21:05 -0300

    Atualizar o Fahrenheit-Celsius.py

--- AlgoritmosdeProgramação/Fahrenheit-Celsius.py
+++ AlgoritmosdeProgramação/Fahrenheit-Celsius.py
@@ -1,3 +1,10 @@
+Converter Fahrenheit para Celsius em Python
+--------------------------------------------------
+# Solicita a temperatura em Fahrenheit
 Fahrenheit = float(input("Digite a temperatura em °F = "))
+
+# Converte para Celsius
 Celsius = (Fahrenheit - 32) * 5 / 9
+
+# Exibe o resultado
 print(f"A temperatura em Celsius é: {Celsius}")
