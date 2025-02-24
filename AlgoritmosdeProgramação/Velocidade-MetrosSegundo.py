55c0b3bd05822882a1499a7c2330df794710f0da
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-02-24 16:19:44 -0300

    Atualizar o Velocidade-MetrosSegundo.py

--- AlgoritmosdeProgramação/Velocidade-MetrosSegundo.py
+++ AlgoritmosdeProgramação/Velocidade-MetrosSegundo.py
@@ -1,2 +1,10 @@
+Converter Km/h para m/s em Python
+---------------------------------------
+# Solicita a velocidade em Km/h
 velocidade = float(input("Digite a velocidade em Km/h= "))
-print(f"A velocidade em Metro por Segundo é {velocidade / 3.6:.2f}")
+
+# Converte para m/s
+velocidade_ms = velocidade / 3.6
+
+# Exibe o resultado
+print(f"A velocidade em Metro por Segundo é {velocidade_ms:.2f}")
