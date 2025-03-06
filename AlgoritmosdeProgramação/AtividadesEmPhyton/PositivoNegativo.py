afe29bd092f208e060db67da937b3586aeefc459
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-03-06 13:15:33 -0300

    Update PositivoNegativo.py

--- AlgoritmosdeProgramação/AtividadesEmPhyton/PositivoNegativo.py
+++ AlgoritmosdeProgramação/AtividadesEmPhyton/PositivoNegativo.py
@@ -1,7 +1,13 @@
+Vê se o Numero é Positivo,Negativo ou nulo
+--------------------------------------------------
+#Pede o Numero
 numero=float(input("Digite o Numero!  "))
+#Vê se é Positivo
 if numero>0:
    print(f"{numero:.0f} é Positivo")
+#Vê se é Negativo
 elif numero<0:
    print(f"{numero:.0f} é Negativo")
+#Vê se é Nulo 
 else:
     print(f"{numero:.0f} é Nulo")
