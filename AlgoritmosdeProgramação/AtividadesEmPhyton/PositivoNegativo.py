1247d76d2d2c0c5fb64d50b0b17227ba6614caa6
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-03-06 13:12:07 -0300

    Add files via upload

---
+++ AlgoritmosdeProgramação/AtividadesEmPhyton/PositivoNegativo.py
@@ -0,0 +1,7 @@
+numero=float(input("Digite o Numero!  "))
+if numero>0:
+   print(f"{numero:.0f} é Positivo")
+elif numero<0:
+   print(f"{numero:.0f} é Negativo")
+else:
+    print(f"{numero:.0f} é Nulo")
