5967d3cd17a085d7c8601aac3705b69aa37553a2
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-03-06 13:22:48 -0300

    Update Par-Impar.py

--- AlgoritmosdeProgramação/AtividadesEmPhyton/Par-Impar.py
+++ AlgoritmosdeProgramação/AtividadesEmPhyton/Par-Impar.py
@@ -1,4 +1,6 @@
-# Solicita ao usuário que digite um número inteiro e converte a entrada para um inteiro
+Vai checar se o número é Par ou Ímpar
+------------------------------------------------------------------------------------------
+# Solicita ao usuário que digite um número inteiro 
 numero = int(input("Digite o Número: "))
 
 # Verifica se o número é divisível por 2 sem deixar resto
