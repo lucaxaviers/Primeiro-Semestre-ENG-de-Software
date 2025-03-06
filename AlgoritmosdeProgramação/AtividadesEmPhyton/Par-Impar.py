a1fe2864de96002871347a10b2c2fc189f892597
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-03-06 13:21:24 -0300

    Add files via upload

---
+++ AlgoritmosdeProgramação/AtividadesEmPhyton/Par-Impar.py
@@ -0,0 +1,11 @@
+# Solicita ao usuário que digite um número inteiro e converte a entrada para um inteiro
+numero = int(input("Digite o Número: "))
+
+# Verifica se o número é divisível por 2 sem deixar resto
+if numero % 2 == 0:
+    # Se a condição for verdadeira, o número é par
+    print(f"O número {numero} é par.")
+else:
+    # Se a condição for falsa, o número é ímpar
+    print(f"O número {numero} é ímpar.")
+
