b67e71306e0e3154e56a2ba7659c16430035c181
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-03-10 11:03:46 -0300

    Add files via upload

---
+++ AlgoritmosdeProgramação/AtividadesEmPhyton/AtribuirValor.py
@@ -0,0 +1,9 @@
+valor1 = int(input("Digite um número: "))
+valor2 = int(input("Digite um número: "))
+
+if valor1 == valor2:
+    valor = valor1 + valor2
+    print(f"A soma é {valor}")
+else:
+    valor = valor1 * valor2
+    print(f"A multiplicação é {valor}")
