bc3e9a59f726d331eb8e5618673e2bef948daf18
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-03-10 11:32:26 -0300

    Update AtribuirValor.py

--- AlgoritmosdeProgramação/AtividadesEmPhyton/AtribuirValor.py
+++ AlgoritmosdeProgramação/AtividadesEmPhyton/AtribuirValor.py
@@ -1,9 +1,17 @@
+# Solicita ao usuário para digitar o primeiro número e armazena na variável 'valor1'
 valor1 = int(input("Digite um número: "))
+
+# Solicita ao usuário para digitar o segundo número e armazena na variável 'valor2'
 valor2 = int(input("Digite um número: "))
 
+# Verifica se os dois números digitados são iguais
 if valor1 == valor2:
+    # Se os números forem iguais, realiza a soma dos dois números e armazena o resultado em 'valor'
     valor = valor1 + valor2
+    # Exibe o resultado da soma
     print(f"A soma é {valor}")
 else:
+    # Se os números forem diferentes, realiza a multiplicação dos dois números e armazena o resultado em 'valor'
     valor = valor1 * valor2
+    # Exibe o resultado da multiplicação
     print(f"A multiplicação é {valor}")
