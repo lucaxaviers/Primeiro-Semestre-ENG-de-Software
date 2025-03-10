a9c933d2e2eb94e51e6b7db7ab943b896bd4c74c
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-03-10 11:33:23 -0300

    Update Múltiplosde7.py

--- AlgoritmosdeProgramação/AtividadesEmPhyton/Múltiplosde7.py
+++ AlgoritmosdeProgramação/AtividadesEmPhyton/Múltiplosde7.py
@@ -1,4 +1,9 @@
+# Exibe a mensagem informando que serão mostrados os múltiplos de 7
 print("Os números que são múltiplos de 7 são:")
+
+# Laço 'for' para percorrer os números de 100 até 450 (inclusive)
 for numero in range(100, 451):
-   if numero % 7 == 0:
-   print(numero)
+    # Verifica se o número é múltiplo de 7 (resto da divisão por 7 é igual a 0)
+    if numero % 7 == 0:
+        # Se a condição for verdadeira, imprime o número
+        print(numero)
