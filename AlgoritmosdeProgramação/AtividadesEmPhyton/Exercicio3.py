8cdb6886e37d961586cd625bd7e9f47589d8064e
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-03-17 12:27:48 -0300

    Atualizar o Exercicio3.py

--- AlgoritmosdeProgramação/AtividadesEmPhyton/Exercicio3.py
+++ AlgoritmosdeProgramação/AtividadesEmPhyton/Exercicio3.py
@@ -1,9 +1,11 @@
 #Escreva um que solicita 10 números ao usuário, e ao final mostre os dois maiores números digitados pelo usuário.
 maior1=0
-maior2=0
+
+#Vai pedir 10 N°s para o usuario
 for cont in range(10):
     num=int(input("Digite um N°: "))
     
+#Confere o maior
     if num > maior1:
         maior2 = maior1  # O maior2 recebe o valor do maior1
         maior1 = num      # O maior1 recebe o valor do número atual
@@ -11,5 +13,6 @@ for cont in range(10):
     elif num > maior2:
         maior2 = num      # Caso o número seja maior que o Maior2
 
+#Mostra o maior e o segundo maior
 print(f"O maior número digitado foi: {maior1}")
 print(f"O segundo maior número digitado foi: {maior2}")
