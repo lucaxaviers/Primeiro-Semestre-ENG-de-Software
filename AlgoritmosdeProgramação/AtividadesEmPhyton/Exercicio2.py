43e9482a27f0d82191454d1d25b31e09a740a12e
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2025-03-17 12:24:30 -0300

    Atualizar o Exercicio2.py

--- AlgoritmosdeProgramação/AtividadesEmPhyton/Exercicio2.py
+++ AlgoritmosdeProgramação/AtividadesEmPhyton/Exercicio2.py
@@ -1,12 +1,12 @@
 # Elabore um programa que leia um número e imprima todos os números divisíveis por 4 que sejam menores que este número lido
 
-# Lê um número do usuário
+# pede um número pro usuário
 numero = int(input("Digite um número: "))
 
-# Imprime os números divisíveis por 4 e menores que o número informado
+# Mostra os números divisíveis por 4 e menores que o número informado
 print(f"Números divisíveis por 4 menores que {numero}:")
 
-# Percorre os números de 1 até o número informado (não incluindo o próprio número)
+# Percorre os números de 1 até o número informado (sem contar ele)
 for i in range(numero):
     # Verifica se o número é divisível por 4
     if i % 4 == 0:
